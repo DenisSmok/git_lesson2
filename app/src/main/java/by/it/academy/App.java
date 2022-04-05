ac064a845f654278223a4658dab967d9d2bb7ea6
Denis_Smok <den1989@inbox.ru>
2022-04-05 10:58:47 +0300

    mult v+n commit

--- app/src/main/java/by/it/academy/App.java
+++ app/src/main/java/by/it/academy/App.java
@@ -10,15 +10,27 @@ public class App {
         System.out.println("First commit");
         System.out.println("sum a+b");
     }
+
     private int a;
     private int b;
-    public int getSum(int a, int b){
-        return a+b;
+
+    public int getSum(int a, int b) {
+        return a + b;
 
     }
+
     private int c;
     private int d;
-    public int getSumcd(int c, int d){
-        return c+d;
 
+    public int getSumcd(int c, int d) {
+        return c + d;
+
+    }
+
+    private int v;
+    private int n;
+
+    public int getmult(int v, int n) {
+        return v * n;
+    }
 }
